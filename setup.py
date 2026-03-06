a1eac10adf72106f0acb7eddd499f1ceb4d88f8d
RaccoNet <zavolokinyegor@gmail.com>
2026-03-06 16:02:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="todo-extract",
+    version="1.0.0",
+    description="Extract TODO/FIXME/HACK comments from code",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/MopicMP/todo-extract",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
